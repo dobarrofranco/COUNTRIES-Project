4c941aed54bdd58cbc7dcbeff4379bcbe1d2c9fb
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-03 20:30:18 -0300

    query handler updated

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const dataFromApi = require("./src/DataFromApi/dataFromApi");
 const PORT = 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
 server.listen(PORT, () => {
 dataFromApi();
 console.log(`Server listening on port ${PORT}`);})
